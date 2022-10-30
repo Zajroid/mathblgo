95f75850780367bb1dc19e2b10af2e5e5c929046
zajroid <zajroid@gmail.com>
2022-10-30 22:13:42 +0300

    add some files)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, Http404, HttpResponseNotFound
+from .models import Post
 
-# Create your views here.
+
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}]
+
+
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница',
+        'categories_selected': 0,
+    }
+
+    return render(request, 'blog/index.html', context=context)
+
+
+def show_category(request, categories_slug):
+    posts = Post.objects.filter(slug=categories_slug)
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': categories_slug,
+    }
+
+    return render(request, 'blog/index.html', context=context)
+
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'categories_selected': post.categories_id,
+    }
+
+    return render(request, 'blog/post.html', context=context)
+
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'menu': menu, 'title': 'About page'})
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена!<h1>')
--- mathblog/settings.py
+++ mathblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^&kw_#rg$0$v#zrim$12kzr4$tpe7g+(i@xqiin-q6mq6btdz8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mathblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mathblog',
+        'USER': 'danik',
+        'PASSWORD': '1251',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
